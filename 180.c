95a7ce655c36d1651b045c6576475c1e4ea39778
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-26 13:57:38 +0530

    Create 180.c

---
+++ 180.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a,b=1,c,rem,s=0,l=0,m=1;
+scanf("%d",&a);
+while(a)
+{
+  rem=a%10;
+   s=s+rem*b;
+  b=b*2;
+   a=a/10;
+}
+while(s>m)
+{
+m=m*2;
+}
+printf("%d",m); 
+return 0; 
+}
